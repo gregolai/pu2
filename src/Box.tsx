56df5e6ff939195a679cf771e7c0b1a292a99da6
Gregory Dalton <gregolai@gmail.com>
2020-07-19 04:37:10 -0400

    p

--- src/Box.tsx
+++ src/Box.tsx
@@ -2,17 +2,28 @@ import React, { forwardRef, useRef } from 'react';
 import cx from './cx';
 import { createParsed, CSSObject, CSSParsedObj } from './exploration/parseCSS';
 import SheetManager from './exploration/SheetManager';
+import { AllProps, allPropsSet } from './exploration/allCSSProps';
 
 const manager = new SheetManager();
 
-export interface BoxProps {
+export interface BoxProps extends Partial<AllProps> {
 	as?: React.ComponentType<any> | string;
-	css: CSSObject;
+	css?: CSSObject;
 	[key: string]: any;
 }
 
-const Box = forwardRef<any, BoxProps>(({ as: Component = 'div', css, ...rest }, ref) => {
+const Box = forwardRef<any, BoxProps>(({ as: Component = 'div', css = {}, ...rest }, ref) => {
 	const prev = useRef<CSSParsedObj>();
+
+	/**
+	 * Apply inline style props to CSS
+	 */
+	css = { ...css };
+	for (const key in rest) {
+		if (allPropsSet.has(key)) {
+			css[key] = rest[key];
+		}
+	}
 
 	const next = createParsed(css, prev.current);
 	if (next.checksum !== prev.current?.checksum) {
