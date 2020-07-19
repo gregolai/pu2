c082d09c382f1d000907accd2d8ec45bdf234491
Gregory Dalton <gregolai@gmail.com>
2020-07-19 05:05:37 -0400

    p

--- src/Box.tsx
+++ src/Box.tsx
@@ -9,45 +9,49 @@ const manager = new SheetManager();
 export interface BoxProps extends Partial<AllProps> {
 	as?: React.ComponentType<any> | string;
 	css?: CSSObject;
+	ref?: React.Ref<HTMLElement>;
+	[key: string]: any;
 }
 
-const Box = forwardRef<any, BoxProps>(({ as: Component = 'div', css = {}, ...rest }, ref) => {
-	const prev = useRef<CSSParsedObj>();
+const Box: React.FC<BoxProps> = forwardRef<HTMLElement, BoxProps>(
+	({ as: Component = 'div', css = {}, ...rest }, ref) => {
+		const prev = useRef<CSSParsedObj>();
 
-	/**
-	 * Apply inline style props to CSS
-	 */
-	const finalCSS = {};
-	for (const key in rest) {
-		if (allPropsSet.has(key)) {
-			// @ts-ignore
-			finalCSS[key] = rest[key];
-			// @ts-ignore
-			delete rest[key];
+		/**
+		 * Apply inline style props to CSS
+		 */
+		const finalCSS = {};
+		for (const key in rest) {
+			if (allPropsSet.has(key)) {
+				// @ts-ignore
+				finalCSS[key] = rest[key];
+				// @ts-ignore
+				delete rest[key];
+			}
 		}
+
+		/**
+		 * Apply CSS on top
+		 */
+		Object.assign(finalCSS, css);
+
+		const next = createParsed(finalCSS, prev.current);
+		if (next.checksum !== prev.current?.checksum) {
+			manager.addOrUpdateObj(next);
+			prev.current = next;
+		}
+
+		return (
+			<Component
+				{...rest}
+				// @ts-ignore
+				className={cx(next.className, rest.className)}
+				ref={ref}
+			>
+				{rest.children}
+			</Component>
+		);
 	}
-
-	/**
-	 * Apply CSS on top
-	 */
-	Object.assign(finalCSS, css);
-
-	const next = createParsed(finalCSS, prev.current);
-	if (next.checksum !== prev.current?.checksum) {
-		manager.addOrUpdateObj(next);
-		prev.current = next;
-	}
-
-	return (
-		<Component
-			{...rest}
-			// @ts-ignore
-			className={cx(next.className, rest.className)}
-			ref={ref}
-		>
-			{rest.children}
-		</Component>
-	);
-});
+);
 
 export default Box;
