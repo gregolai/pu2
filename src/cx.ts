a05a9a68982bf6ed2799d2aec633b2dbb257af68
Gregory Dalton <gregolai@gmail.com>
2020-12-06 11:17:36 -0700

    p

--- src/cx.ts
+++ src/cx.ts
@@ -5,20 +5,38 @@ const cx = (...args: CxClassName[]) => {
 
 	for (let i = 0, ii = args.length; i < ii; ++i) {
 		const arg = args[i];
-		if (!arg) continue; // No falsy args
+
+		/**
+		 * No falsy args
+		 */
+		if (!arg) continue;
 
 		if (typeof arg === 'string') {
+			/**
+			 * e.g. "my-button"
+			 */
 			classes.push(arg);
 		} else if (Array.isArray(arg)) {
-			const inner = cx(...arg);
-			if (inner) classes.push(inner);
+			/**
+			 * e.g. ["my-button", "primary"]
+			 */
+			if (arg.length) {
+				const inner = cx(...arg);
+				if (inner) classes.push(inner);
+			}
 		} else if (typeof arg === 'object') {
+			/**
+			 * e.g. { "my-button": true, "primary": true, "secondary": false }
+			 */
 			for (const key in arg) {
 				if (arg[key]) classes.push(key);
 			}
 		}
 	}
 
+	/**
+	 * e.g. "my-button primary"
+	 */
 	return classes.join(' ');
 };
 
