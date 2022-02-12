6a218150fe632acc5b42841c5534c030855cd08c
Gregory Dalton <gregolai@gmail.com>
2022-02-12 17:28:04 -0600

    p

--- src/style-lib/browser/Box.tsx
+++ src/style-lib/browser/Box.tsx
@@ -4,31 +4,38 @@ import { useStyle } from './useStyle';
 import { StyleProps, isStyleProp } from '../internal/style-props.generated';
 import { CSSInput } from '../internal/parser';
 
+type AsComponent<T = any> =
+	| React.FunctionComponent<T>
+	| React.ComponentClass<T>
+	| keyof JSX.IntrinsicElements;
+
 export type BoxProps = Partial<StyleProps> & {
-	as?: React.FunctionComponent<any> | React.ComponentClass<any> | keyof JSX.IntrinsicElements;
+	as?: AsComponent;
 	className?: Parameters<typeof cx>[0];
 	css?: CSSInput;
 	ref?: React.Ref<unknown>;
 	[key: string]: any;
 };
 
-export const Box = forwardRef<unknown, BoxProps>(({ as: Component = 'div', css: extraCSS, ...rest }, ref) => {
-	/**
-	 * Apply inline style props to CSS
-	 * e.g. <Box backgroundColor="green" color="white">
-	 */
-	const css: CSSInput = {};
-	for (const key in rest) {
-		if (isStyleProp(key)) {
-			css[key] = rest[key];
-			delete rest[key];
+export const Box: React.FC<BoxProps> = forwardRef<unknown, BoxProps>(
+	({ as: Component = 'div', css: extraCSS, ...rest }, ref) => {
+		/**
+		 * Apply inline style props to CSS
+		 * e.g. <Box backgroundColor="green" color="white">
+		 */
+		const css: CSSInput = {};
+		for (const key in rest) {
+			if (isStyleProp(key)) {
+				css[key] = rest[key];
+				delete rest[key];
+			}
 		}
+
+		/**
+		 * Apply CSS on top
+		 */
+		Object.assign(css, extraCSS);
+
+		return <Component ref={ref} {...rest} className={cx(useStyle(css), rest.className)} />;
 	}
-
-	/**
-	 * Apply CSS on top
-	 */
-	Object.assign(css, extraCSS);
-
-	return <Component ref={ref} {...rest} className={cx(useStyle(css), rest.className)} />;
-});
+);
